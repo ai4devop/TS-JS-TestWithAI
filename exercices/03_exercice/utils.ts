93a0e567e0b82ee5070a5e80ba1f4a5ed2f1cbd4
ai4devop <ai4dev@groupeonepoint.com>
2025-02-11 09:44:14 +0100

    Delete exercices directory

--- exercices/03_exercice/utils.ts
+++
@@ -1,8 +0,0 @@
-// Nouvelle fonction asynchrone simulant un appel API (mock)
-export async function fetchData(): Promise<{ data: string }> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ data: "Hello, world!" });
-    }, 1000); // Simule le délais du réseau
-  });
-}
